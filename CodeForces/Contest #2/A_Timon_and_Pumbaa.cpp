9f601e7b027d608137ebed61bafb88a58b4b581c
sheikh-mohammad-rakib <u2009023@student.cuet.ac.bd>
2023-07-14 13:47:22 +0600

    test

---
+++ CodeForces/Contest #2/A_Timon_and_Pumbaa.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b; cin>>a>>b;
+
+    if(a>=b){
+        cout<<a-b<<"\n";
+    }
+    else{
+        cout<<"0"<<"\n";
+    }
+    return 0;
+}
